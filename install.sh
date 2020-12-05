c4a9686bc8b4319e08836650eff78eece477f7de
Thrada <mjnhb2001@gmail.com>
2020-12-05 15:13:00 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo chmod -R 755 ./
 
 sudo apt update
 
-sudo apt install build-essential autoconf libtool python3-dev python3-sip-dev cmake -y
+sudo apt install -y build-essential autoconf libtool python3-dev python3-sip-dev cmake
 
 cd protobuf-3.11.4
 
